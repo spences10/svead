fc978f534d65cbd425b7dc28563f8c6dc7251812
spences10 <spences10apps@gmail.com>
2022-05-15 20:12:59 +0100

    refactor: :recycle: add tailgraph open graph image

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,27 +1,19 @@
 <script>
+  import { page } from '$app/stores'
   import { Head } from 'svead'
 
   let title = 'This is Svead a Svelte Head Component'
   let description =
     'Svead, a component that allows you to set head meta information, canonical, title, Twitter and Facebook Open Graph tags.'
-  let image = ''
-  let url = `https://og.tailgraph.com/og
-    ?fontFamily=Roboto
-    &title=This%20is%20Svead
-    &titleTailwind=text-gray-800%20font-bold%20text-6xl
-    &text=Set%20Head%20meta%20tag%20information
-    &textTailwind=text-gray-700%20text-2xl%20mt-4
-    &logoTailwind=h-8
-    &bgTailwind=bg-white
-    &footer=tailgraph.com
-    &footerTailwind=text-teal-600`
-  let website = ''
+  let image = `https://og.tailgraph.com/og?fontFamily=Roboto&title=This+is+Svead&titleTailwind=text-gray-800+font-bold+text-6xl&text=Set+Head+meta+tag+information&textTailwind=text-gray-700+text-2xl+mt-4&logoTailwind=h-8&bgTailwind=bg-white&footer=tailgraph.com&footerTailwind=text-teal-600`
+  let website = 'https://svead.vercel.app/'
+  let url = $page.url.toString()
 </script>
 
 <Head {title} {description} {image} {url} {website} />
 
-<h1>Welcome to SvelteKit</h1>
+<h1>Welcome to Svead</h1>
 <p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the
-  documentation
+  Visit <a href="https://github.com/spences10/svead">GitHub</a> to contrubute
+  to this project.
 </p>
