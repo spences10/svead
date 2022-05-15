cd7b46372fad2f7b2622ed127f9ded49c1cf0f3b
spences10 <spences10apps@gmail.com>
2022-05-15 15:10:07 +0100

    refactor: :recycle: use variables for props

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,14 +1,24 @@
 <script>
   import { Head } from 'svead'
+
+  let title = 'This is Svead a Svelte Head Component'
+  let description =
+    'Svead, a component that allows you to set head meta information, canonical, title, Twitter and Facebook Open Graph tags.'
+  let image = ''
+  let url = `https://og.tailgraph.com/og
+    ?fontFamily=Roboto
+    &title=This%20is%20Svead
+    &titleTailwind=text-gray-800%20font-bold%20text-6xl
+    &text=Set%20Head%20meta%20tag%20information
+    &textTailwind=text-gray-700%20text-2xl%20mt-4
+    &logoTailwind=h-8
+    &bgTailwind=bg-white
+    &footer=tailgraph.com
+    &footerTailwind=text-teal-600`
+  let website = ''
 </script>
 
-<Head
-  title={'Hello World'}
-  description=""
-  image=""
-  url=""
-  website=""
-/>
+<Head {title} {description} {image} {url} {website} />
 
 <h1>Welcome to SvelteKit</h1>
 <p>
