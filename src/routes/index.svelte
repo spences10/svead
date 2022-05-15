850f1ac54c38643df9c5f151db8e20d8b9ca6ecf
spences10 <spences10apps@gmail.com>
2022-05-15 14:38:06 +0100

    feat: :sparkles: use head component

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,17 @@
+<script>
+  import { Head } from 'svead'
+</script>
+
+<Head
+  title={'Hello World'}
+  description=""
+  image=""
+  url=""
+  website=""
+/>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the
+  documentation
+</p>
