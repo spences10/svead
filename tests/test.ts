d49651a85c8b4cffcf71e5d970f0886d3e3bea28
spences10 <spences10apps@gmail.com>
2022-10-28 16:50:31 +0100

    test: :white_check_mark: add meta tag tests

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,28 @@
 import { expect, test } from '@playwright/test'
+let pageURL = 'http://localhost:4173/'
 
-test('index page has expected h1', async ({ page }) => {
+test('index page has h1', async ({ page }) => {
   await page.goto('/')
   expect(await page.textContent('h1')).toBe('Welcome to Svead')
+})
+
+test('head has canonical', async ({ page }) => {
+  await page.goto('/')
+  const metaDescription = page.locator('link[rel="canonical"]')
+  await expect(metaDescription).toHaveAttribute('href', pageURL)
+})
+
+test('head has description', async ({ page }) => {
+  await page.goto('/')
+  const metaDescription = page.locator('meta[name="description"]')
+  await expect(metaDescription).toHaveAttribute(
+    'content',
+    'Svead, a component that allows you to set head meta information, canonical, title, Twitter and Facebook Open Graph tags.'
+  )
+})
+
+test('has open graph info', async ({ page }) => {
+  await page.goto('/')
+  const metaDescription = page.locator('meta[property="og:type"]')
+  await expect(metaDescription).toHaveAttribute('content', 'website')
 })
