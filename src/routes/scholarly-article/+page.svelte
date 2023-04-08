032b76ca6eade4478e0ea7f0500122fc6e73eb72
spences10 <spences10apps@gmail.com>
2023-04-08 20:26:52 +0100

    refactor: :recycle: add test page routes

---
+++ src/routes/scholarly-article/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import Head from '$lib/components/head.svelte'
+</script>
+
+<Head
+	url="https://example.com/scholarly-article"
+	title="Scholarly Article"
+	description="This is a sample scholarly article."
+	contentType="ScholarlyArticle"
+	authorName="Dr. Jane Smith"
+	authorUrl="https://example.com/authors/dr-jane-smith"
+	image="https://example.com/images/scholarly-article-image.jpg"
+	datePublished="2023-04-05T10:00:00Z"
+	dateModified="2023-04-05T12:00:00Z"
+/>
---
+++ src/routes/tech-article/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import Head from '$lib/components/head.svelte'
+</script>
+
+<Head
+	url="https://example.com/tech-article"
+	title="Tech Article"
+	description="This is a sample tech article."
+	contentType="TechArticle"
+	authorName="John Techwriter"
+	authorUrl="https://example.com/authors/john-techwriter"
+	image="https://example.com/images/tech-article-image.jpg"
+	datePublished="2023-04-05T10:00:00Z"
+	dateModified="2023-04-05T12:00:00Z"
+/>
