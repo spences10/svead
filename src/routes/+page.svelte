53a1a6b833ca975659bcfc7c1a56e663db85f8fe
spences10 <spences10apps@gmail.com>
2023-04-08 20:25:38 +0100

    refactor: :recycle: move index back to svelte file
    import copy for index, update types

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { page } from '$app/stores'
+	import Head from '$lib/components/head.svelte'
+
+	export let data
+	let { Copy } = data
+
+	let title = 'This is Svead a Svelte Head Component'
+	let description =
+		'Svead, a component that allows you to set head meta information, canonical, title, Twitter and Facebook Open Graph tags.'
+	let image = `https://og.tailgraph.com/og?fontFamily=Roboto&title=This+is+Svead&titleTailwind=text-gray-800+font-bold+text-6xl&text=Set+Head+meta+tag+information&textTailwind=text-gray-700+text-2xl+mt-4&logoTailwind=h-8&bgTailwind=bg-white&footer=svead.vercel.app&footerTailwind=text-teal-600`
+	let website = 'https://svead.vercel.app/'
+	let url = $page.url.toString()
+</script>
+
+<Head {title} {description} {image} {url} {website} />
+
+<svelte:component this={Copy} />
--- src/types.ts
+++ src/types.ts
@@ -1,93 +1,38 @@
-interface MainEntityBase {
-	WebPage: string
-	Article: string
-	Blog: string
-	NewsArticle: string
-	ScholarlyArticle: string
-	TechArticle: string
-	QAPage: string
-	ItemPage: string
-	CollectionPage: string
-	AboutPage: string
-	ContactPage: string
-	ProfilePage: string
-	SearchResultsPage: string
-	Event: string
-	Recipe: string
-	Review: string
-	JobPosting: string
-	Product: string
-	LocalBusiness: string
-	Organization: string
-	Person: string
-	CreativeWork: string
+export type MainEntityType =
+	| 'WebPage'
+	| 'Article'
+	| 'Blog'
+	| 'BlogPosting'
+	| 'NewsArticle'
+	| 'ScholarlyArticle'
+	| 'TechArticle'
+
+export type AuthorType = 'Person' | 'Organization'
+
+interface CommonEntity {
+	name: string
+	url: string
+	headline: string
+	description: string
+	image: string
+	datePublished: string
+	dateModified: string
 }
 
-interface MainEntityAdditional {
-	VideoObject: string
-	Movie: string
-	MusicRecording: string
-	AudioObject: string
-	SoftwareApplication: string
-	MobileApplication: string
-	WebApplication: string
-	Book: string
-	Course: string
-	HowTo: string
-	FAQPage: string
-	Service: string
-	Dataset: string
+export interface AuthorEntity {
+	type: AuthorType
+	name: string
+	url: string
 }
 
-/**
- * The type for specifying the main entity type of the content.
- * Possible values include 'Article', 'Blog', 'NewsArticle', 'Recipe', etc.
- */
-export type MainEntityType = keyof (MainEntityBase &
-	MainEntityAdditional)
-
-/**
- * The type for specifying the content type for JSON-LD.
- * Possible values include 'WebPage', 'Article', 'Blog', 'NewsArticle', etc.
- */
-export type ContentType = keyof (MainEntityBase &
-	MainEntityAdditional)
-
-interface OrganizationBase {
-	Organization: string
-	Corporation: string
-	EducationalOrganization: string
-	GovernmentOrganization: string
-	LocalBusiness: string
-	NonprofitOrganization: string
-	SportsOrganization: string
+export interface PublisherEntity {
+	type: 'Organization'
+	name: string
+	logo: string
 }
 
-interface OrganizationAdditional {
-	NGO: string
-	PerformingGroup: string
-	SportsTeam: string
-	MusicalGroup: string
-	BroadcastService: string
-	RadioStation: string
-	TelevisionStation: string
+export interface MainEntity extends CommonEntity {
+	type: MainEntityType
+	author: AuthorEntity
+	publisher: PublisherEntity
 }
-
-interface PersonType {
-	Person: string
-}
-
-/**
- * The type for specifying the author of the content.
- * Possible values include 'Person', 'Organization', 'Corporation', etc.
- */
-export type AuthorType = keyof (PersonType &
-	OrganizationBase &
-	OrganizationAdditional)
-
-/**
- * The type for specifying the publisher of the content.
- * Possible values include 'Organization', 'Corporation', 'EducationalOrganization', etc.
- */
-export type PublisherType = keyof (OrganizationBase &
-	OrganizationAdditional)
