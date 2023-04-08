1f4c3237cc4571fa0656082e80c0614c9f5bc0c2
spences10 <spences10apps@gmail.com>
2023-04-08 20:26:29 +0100

    refactor: :recycle: update playwrigt config

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,10 +2,25 @@ import type { PlaywrightTestConfig } from '@playwright/test'
 
 const config: PlaywrightTestConfig = {
 	webServer: {
-		command: 'npm run build && npm run preview',
+		command: 'pnpm run build && pnpm run preview',
 		port: 4173,
+		timeout: 2 * 60 * 1000,
 	},
 	testDir: 'tests',
+	projects: [
+		{
+			name: 'Chromium',
+			use: { browserName: 'chromium' },
+		},
+		{
+			name: 'Firefox',
+			use: { browserName: 'firefox' },
+		},
+		{
+			name: 'WebKit',
+			use: { browserName: 'webkit' },
+		},
+	],
 }
 
 export default config
