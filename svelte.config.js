4b8202ddcd5a137be8eb24dd90abd4767ac23896
spences10 <spences10apps@gmail.com>
2023-08-05 15:07:19 +0100

    revert: :rewind: reeewinnnnd

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-vercel'
+import adapter from '@sveltejs/adapter-auto'
 import { vitePreprocess } from '@sveltejs/kit/vite'
 import { mdsvex } from 'mdsvex'
 import mdsvexConfig from './mdsvex.config.js'
